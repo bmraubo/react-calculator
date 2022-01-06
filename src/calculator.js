ccfbdc869d9484cd7ebb49018ffd2b26aac67634
bmraubo <bmraubo@gmail.com>
2022-01-06 14:47:35 +0000

    changes to make React tests run

---
+++ src/calculator.js
@@ -0,0 +1,87 @@
+import React from "react";
+import Calculate from "./calculate";
+
+const calculate = new Calculate();
+const operations = {
+    "X": calculate.multiply,
+    "+": calculate.add,
+    "-": calculate.subtract,
+    "/": calculate.divide,
+    "=": calculate.calculate,
+};
+
+function NumberButton(props) {
+    return (
+        <button>
+            {props.value}
+        </button>
+    );
+}
+
+function OperationsButton(props) {
+    return (
+        <button>
+            {props.value}
+        </button>
+    );
+}
+
+export default class Calculator extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            firstNumber: 0,
+            operation: null,
+            secondNumber: 0,
+            result: 0,
+        };
+    }
+
+    renderNumberButton(value) {
+        return (
+            <NumberButton value={value} />
+        );
+    }
+
+    renderOperationsButton(value) {
+        return (
+            <OperationsButton value={value} />
+        );
+    }
+
+    render() {
+        return (
+            <div>
+                <h3 data-testid="Display">
+                    {this.state.result}
+                </h3>
+                <div>
+                    {this.renderOperationsButton("C")}
+                    {this.renderOperationsButton("/")}
+                    {this.renderOperationsButton("X")}
+                    {this.renderOperationsButton("-")}
+                </div>
+                <div>
+                    {this.renderNumberButton(7)}
+                    {this.renderNumberButton(8)}
+                    {this.renderNumberButton(9)}
+                    {this.renderOperationsButton("+")}
+                </div>
+                <div>
+                    {this.renderNumberButton(4)}
+                    {this.renderNumberButton(5)}
+                    {this.renderNumberButton(6)}
+                    {this.renderOperationsButton("=")}
+                </div>
+                <div>
+                    {this.renderNumberButton(1)}
+                    {this.renderNumberButton(2)}
+                    {this.renderNumberButton(3)}
+                    {this.renderNumberButton(0)}                  
+                </div>
+            </div>
+        );
+    }
+}
+
+export {Calculator};
---
+++ src/index.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {render, fireEvent, screen} from '@testing-library/react';
+import '@testing-library/jest-dom';
+import {Calculator} from "./calculator"
+import { unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
+
+describe("Testing Frontend", () => {
+    let container = null;
+    beforeEach(()=> {
+        container = document.createElement("div");
+        document.body.appendChild(container);
+    });
+
+    afterEach(() => {
+        unmountComponentAtNode(container);
+        container.remove();
+        container = null;
+    })
+
+    it("Pressing = returns 0", () => {
+        act(() => {
+            render(
+                <Calculator />,
+                container
+            )
+        })
+        fireEvent.click(screen.getByText("="));
+        expect(screen.getByTestId("Display").data).toBe(0);
+    });
+});
