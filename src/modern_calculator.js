1df619f337503890916b6b7fd5f6a9e6132803e9
bmraubo <bmraubo@gmail.com>
2022-01-07 10:13:15 +0000

    clean up

--- src/modern_calculator.js
+++ src/modern_calculator.js
@@ -1,4 +1,3 @@
-import React, { useEffect } from "react";
 import { useState } from "react";
 import Calculate from "./calculate";
 
@@ -6,8 +5,6 @@ const calculate = new Calculate();
 
 export default function ModernCalculator() {
     const [display, setDisplay] = useState("0")
-    const [firstNumber, setFirstNumber] = useState(0)
-    const [secondNumber, setSecondNumber] = useState(0)
     const [operation, setOperation] = useState("+")
     
 
@@ -17,8 +14,6 @@ export default function ModernCalculator() {
         "-": calculate.subtract,
         "/": calculate.divide,
     };
-
-    useEffect(() => console.log("to hell with react"))
 
     function Display({value}) {
         return <h3 data-testid="Display">{value}</h3>
@@ -38,28 +33,16 @@ export default function ModernCalculator() {
 
     function OperationsButton({value}) {
         return <button onClick={() => {
-            // lock in firstNumber
-            console.log(display)
-            // lock in operation
             setOperation(value)
-            // display operation space operation sign space
             setDisplay(display+value)
-            console.log("Operation: " + operation)
         }}>{value}</button>
     }
     
     function EqualsButton({value}) {
         return <button onClick={() => {
-            console.log("Equals Display: " + display)
-            // lock in second number (will have to split string)
             var numbers = readDisplay()
             setDisplay(display)
-            console.log(numbers)
-            console.log("Equals First Number: " + numbers[0])
-            console.log("Equals Second Number: " + numbers[1])
-            // send data to calculate function and display result
             var result = calculate.calculate(numbers[0], numbers[1], operations[operation])
-            console.log("Result: " + result)
             setDisplay(result)
         }}>{value}</button>
     }
@@ -73,7 +56,6 @@ export default function ModernCalculator() {
 
     function readDisplay() {
         let splitDisplay = display.split(operation)
-        console.log(splitDisplay)
         if (splitDisplay.length < 2) {
             return [parseInt(splitDisplay[0]), 0]
         } else {
